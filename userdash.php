1c40c63b2e4a958a2928dfa60799feb426671707
ak4t7m4 <amitsahani@eoxysit.com>
2022-10-14 18:16:45 +0530

    v1

--- userdash.php
+++ userdash.php
@@ -1,10 +1,8 @@
 <?php
 include('header.php');
 session_start();
-if(isset($_SESSION['data']['id'])){
-    if($_SESSION['data']['role']=='admin' && $_SESSION['data']['id']=="1"){
-        header("location: admindash.php");
-    }
+if(!isset($_SESSION['data']['id'])){
+    header("location: index.php");
 }
 $uid = $_SESSION['data']['id'];
 $sql = "select * from users where id = '$uid'";
